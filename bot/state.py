a4d9f08c8d3cd9c5af647350dd541e5a76cb6530
_Atoo_o_ <Nightoo.too@yandex.ru>
2026-05-06 08:19:07 +0500

    Delete bot directory

--- bot/state.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-from threading import Lock
-
-state_lock = Lock()
-user_states = {}                     # {chat_id: {action: ..., ...}}
-user_histories = defaultdict(list)   # {user_id: [messages]}
-user_system_prompts = {}             # {user_id: str}
-user_last_activity = {}              # {user_id: timestamp}
