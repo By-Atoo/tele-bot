ab08181355c4e5d01bbf329d8a52e5e2d84fd71c
_Atoo_o_ <Nightoo.too@yandex.ru>
2026-05-06 10:42:53 +0500

    Update state.py

--- bot/state.py
+++ bot/state.py
@@ -1,3 +1,4 @@
+# state.py
 from collections import defaultdict
 from threading import Lock
 
@@ -6,9 +7,6 @@ user_states = {}
 user_histories = defaultdict(list)
 user_system_prompts = {}
 user_last_activity = {}
-
 bot_messages = defaultdict(list)
-
 banned_users = set()
-
 cooldowns = {}
