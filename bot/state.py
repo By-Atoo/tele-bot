0a884c9a8c572dd8f2cbf1fe0d8ec0f7cfed3bde
_Atoo_o_ <Nightoo.too@yandex.ru>
2026-05-06 08:20:57 +0500

    Add files via upload

---
+++ bot/state.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+from threading import Lock
+
+state_lock = Lock()
+user_states = {}
+user_histories = defaultdict(list)
+user_system_prompts = {}
+user_last_activity = {}
+
+bot_messages = defaultdict(list)
+
+banned_users = set()
+
+cooldowns = {}
